979bffb162f6500b0217dc65de0c4e52927f972e
SergioDegas <104730479+SergioDegas@users.noreply.github.com>
2022-10-03 11:48:55 +0000

    Deploying to gh-pages from @ SergioDegas/goit-js-hw-09@279c918396c016a238f0b6530fceec574e8c6ac3 🚀

---
+++ 01-color-switcher.df9a9660.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0})),t.addEventListener("click",(function(){n=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1}));var d=document.querySelector("body"),n=null;t.disabled=!1,e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.df9a9660.js.map
